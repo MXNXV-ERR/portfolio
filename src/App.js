ce286e6b1f5d14a0c316e0435f802818db9c0a49
MXNXV-ERR <manavrai.jmn@gmail.com>
2023-06-29 16:25:01 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import PortfolioPage from './PortfolioPage';
+import React, { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    document.title = "Manav`s portfolio"; // Change this to your desired title
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   PortfolioPage()
   );
 }
 
---
+++ src/PortfolioPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+const PortfolioPage = () => {
+  return (
+    <div>
+      <header>
+        <h1>MXNXV-ERR</h1>
+      </header>
+      <nav>
+      
+        
+      </nav>
+    
+      <footer>
+        <p>&copy; 2023 MXNXV-ERR. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+};
+
+export default PortfolioPage;
